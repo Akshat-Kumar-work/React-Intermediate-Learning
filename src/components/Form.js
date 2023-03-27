0be30c7c1edba95ef2c784ea1d9af02efbbf7e1d
Akshat <iamakku0.0.0.1@gmail.com>
2023-03-27 22:02:16 +0530

    commitng on 27 march 23

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,24 +20,33 @@ const[formData , setformData] = useState(
        firstName:"",
        lastName:"",
        email:"",
-       Comments:""
+       Comments:"",
+       isVisible:false,
+       mode:"",
+       favCar:""
    }
                      );
 
 
-
-
 function changeHandler (event){
+
 //isme previous state jo hai form data jisek andar object hai usko 
 // copy kar rhe hai aur jis element m trigger hua hai event uski value ko bhi update kr rhe hai using spread operator
-setformData((prevFormData) =>{ return ({...prevFormData,[event.target.name]:event.target.value})   } );
+//event jis target par hua hai agar uske name ka type check box hai toh usme checked or unchecked dalo aur agar type checkbox nahi hai toh value dalo
+setformData((prevFormData) =>{ return ({...prevFormData,[event.target.name]:event.target.type ==="checkbox"? event.target.checked : event.target.value })   } );
     
 }
-console.log(formData)
+
+
+function submitHandler (event){
+    event.preventDefault();
+    console.log("printing submited form data")
+    console.log(formData)
+}
 
     return(
         <div>
-<form>
+<form onSubmit={submitHandler}>
      <input type="text" placeholder='first name' onChange={changeHandler} name="firstName" value={formData.firstName} />
 
      <br></br>
@@ -56,6 +65,47 @@ console.log(formData)
 
      <textarea placeholder='enter your comments here' onChange={changeHandler} name="Comments" value={formData.Comments}
      ></textarea>
+
+     <br></br>
+     <br></br>
+
+     <input type="checkbox" onChange={changeHandler} name="isVisible"  id="isvisible" checked={formData.isVisible}
+     ></input>
+     <label htmlFor='isvisible'>Am i visible?</label>
+
+     <br></br>
+     <br></br>
+
+     <input type='radio' onChange={changeHandler} name="mode"  id="online-mode" 
+     checked={formData.mode === "Online-Mode" }
+     value="Online-Mode"  ></input>
+     <label htmlFor='online-mode' >online mode</label>
+
+     <input type='radio' onChange={changeHandler} name="mode"  id="offline-mode"
+      checked={formData.mode === "Offline-Mode" } 
+     value="Offline-Mode"></input>
+     <label htmlFor='online-mode' >offline mode</label>
+
+     <br></br>
+     <br></br>
+
+     <label htmlFor='favCar'>select fav car</label>
+     <select onChange={changeHandler} name="favCar" id='favCar' value={formData.value}>
+
+     <option value="fortuner">fortuner</option>
+     <option value="bmw">bmw</option>
+     <option value="ferari">ferari</option>
+     <option value="thar">thar</option>
+
+     </select>
+
+     <br></br>
+     <br></br>
+     
+    <button >
+        submit
+    </button>
+
      </form>
         </div>
     )
